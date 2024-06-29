76d80bfab321273c9bc82433751ee85e7a4fd370
hacccico <hacccico@gmail.com>
2024-06-30 06:52:33 +0900

    イベント処理の課題追加

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  const changeBtn = document.getElementById('text');
+  changeBtn.textContent ='ボタンをクリックしました';
+
+});
